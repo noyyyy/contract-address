f8402c6edc78954167b1c6ac8ae303e7ca765b01
Noy <noy_gpg.2fcap@simplelogin.com>
2023-01-19 22:31:14 +0800

    ✨ feat: add create2

--- contracts/commonFactory.sol
+++ contracts/commonFactory.sol
@@ -20,4 +20,40 @@ contract CommonFactory {
     }
     emit ContractCreated(addr);
   }
+
+  function deployCommonByCreate2() external {
+    bytes32 salt = bytes32(uint256(1));
+    Common addr = new Common{ salt: salt }();
+
+    emit ContractCreated(address(addr));
+  }
+
+  function deployCommonByCreate2Assembly() external {
+    address addr;
+    bytes memory bytecode = type(Common).creationCode;
+    bytes32 salt = bytes32(uint256(1));
+
+    assembly {
+      addr := create2(0, add(bytecode, 32), mload(bytecode), salt)
+    }
+    emit ContractCreated(addr);
+  }
+
+  function deployCommonByCreate2Assembly(bytes memory bytecode) external {
+    address addr;
+    bytes32 salt = bytes32(uint256(1));
+
+    assembly {
+      addr := create2(0, add(bytecode, 32), mload(bytecode), salt)
+    }
+    emit ContractCreated(addr);
+  }
+
+  function deployContractByCreate2Assembly(bytes memory bytecode, bytes32 salt) external {
+    address addr;
+
+    assembly {
+      addr := create2(0, add(bytecode, 32), mload(bytecode), salt)
+    }
+  }
 }
